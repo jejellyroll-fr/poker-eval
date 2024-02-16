0dc330447ccc86c5c93e33bb078b58147b05b8f8
jejellyroll-fr <jejellyroll.fr@gmail.com>
2024-02-16 16:27:52 +0100

    try fix build windows

--- examples/five_card_hands.c
+++ examples/five_card_hands.c
@@ -20,108 +20,101 @@
  */
 
 
-#include	<stdio.h>
-#include	<stdlib.h>
-#include	<string.h>
-#include	<signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
 
-#include	"poker_defs.h"
-#if defined(_MSC_VER) || defined(__MINGW32__) || defined( __BCPLUSPLUS__)  || defined( __MWERKS__)
-#include	"getopt_w32.h"
+#include "poker_defs.h"
+
+// Utiliser getopt_w32.h pour Windows et getopt.h pour les autres systèmes
+#if defined(_MSC_VER) || defined(__MINGW32__) || defined(__BCPLUSPLUS__) || defined(__MWERKS__)
+#include "getopt_w32.h"
 #else
-#include        <getopt.h>
+#include <getopt.h>
 #endif
-#include	"inlines/eval.h"
 
-/* #define VERBOSE */
+#include "inlines/eval.h"
 
-uint32 totals[HandType_LAST+1];
+// Définir VERBOSE pour obtenir un affichage détaillé, sinon commenter
+// #define VERBOSE
+
+uint32 totals[HandType_LAST + 1];
 int num_dead;
 CardMask dead_cards;
 
 const char *opts = "d:";
 
 /*
- * returns number of dead cards, -1 on error
- *
- * argc, argv are the standard arguments to main
- * num_dead is a return value of the number of dead cards
- * dead_cards is a return value of the dead cards
+ * Parse les arguments de la ligne de commande.
+ * Retourne le nombre de cartes mortes, ou -1 en cas d'erreur.
  */
-static int
-parse_args(int argc, char **argv, int *num_dead, CardMask *dead_cards) {
-  int i, c, o, rc, len;
+static int parse_args(int argc, char **argv, int *num_dead, CardMask *dead_cards) {
+    int i, c, o, rc, len;
 
-  if (num_dead == NULL) {
-    return (-1);
-  }
-
-  if (dead_cards == NULL) {
-    return (-1);
-  }
-
-  *num_dead = 0;
-  CardMask_RESET(*dead_cards);
-
-  /*
-   * parse any options passed to us.
-   * -d "c1 [...]" is dead cards
-   */
-  while((o = getopt(argc, argv, opts)) != -1) {
-    switch(o) {
-    case 'd':
-      len = strlen(optarg);
-      for(i = 0;i < len;) {
-	rc = StdDeck_stringToCard(optarg+i, &c);
-	if (rc) {
-	  StdDeck_CardMask_SET(*dead_cards, c);
-	  (*num_dead)++;
-	  i += 2;
-	} else {
-	  i++;
-	}
-      }
-      break;
+    if (num_dead == NULL || dead_cards == NULL) {
+        return -1;
     }
-  }
 
-  return (*num_dead);
+    *num_dead = 0;
+    CardMask_RESET(*dead_cards);
+
+    while ((o = getopt(argc, argv, opts)) != -1) {
+        switch (o) {
+            case 'd':
+                len = strlen(optarg);
+                for (i = 0; i < len;) {
+                    rc = StdDeck_stringToCard(optarg + i, &c);
+                    if (rc) {
+                        StdDeck_CardMask_SET(*dead_cards, c);
+                        (*num_dead)++;
+                        i += 2; // Passer à la carte suivante
+                    } else {
+                        i++;
+                    }
+                }
+                break;
+            default:
+                return -1; // Option non reconnue
+        }
+    }
+
+    return *num_dead;
 }
 
 void dump_totals(void) {
-  int i;
-  
-  for (i = HandType_FIRST; i <= HandType_LAST; i++)
-    printf("%s:  %d\n", handTypeNamesPadded[i], totals[i]);
+    int i;
+    for (i = HandType_FIRST; i <= HandType_LAST; i++) {
+        printf("%s: %d\n", handTypeNamesPadded[i], totals[i]);
+    }
 }
 
-
 #ifdef VERBOSE
-#define DUMP_HAND do {                          \
-  Deck_printMask(cards);                        \
-  printf(": ");                                 \
-  HandVal_print(handval);                       \
-  printf("\n");                                 \
+#define DUMP_HAND do { \
+    Deck_printMask(cards); \
+    printf(": "); \
+    HandVal_print(handval); \
+    printf("\n"); \
 } while (0)
 #else
 #define DUMP_HAND do { } while (0)
 #endif
 
-int main(int argc, char *argv[])
-{
-  CardMask cards, dead_cards;
-  HandVal  handval;
-  int      num_dead;
+int main(int argc, char *argv[]) {
+    CardMask cards;
+    HandVal handval;
 
-  parse_args(argc, argv, &num_dead, &dead_cards);
+    if (parse_args(argc, argv, &num_dead, &dead_cards) == -1) {
+        fprintf(stderr, "Erreur lors de l'analyse des arguments.\n");
+        exit(EXIT_FAILURE);
+    }
 
-  ENUMERATE_5_CARDS_D(cards, dead_cards,
-                    {
-                      handval = Hand_EVAL_N(cards, 5);
-                      ++totals[HandVal_HANDTYPE(handval)];
-                      DUMP_HAND;
-                    });
+    ENUMERATE_5_CARDS_D(cards, dead_cards, {
+        handval = Hand_EVAL_N(cards, 5);
+        ++totals[HandVal_HANDTYPE(handval)];
+        DUMP_HAND;
+    });
 
-  dump_totals();
-  exit(0);
+    dump_totals();
+    exit(EXIT_SUCCESS);
 }
--- examples/getopt_w32.h
+++ examples/getopt_w32.h
@@ -1,12 +1,32 @@
 #ifndef __GETOPT_W32_H__
 #define __GETOPT_W32_H__
 
-/* LICENSE: see LICENSE.getopt at the top-level directory. */
+/* 
+ * LICENSE: see LICENSE.getopt at the top-level directory. 
+ *
+ * This header provides declarations for a cross-platform getopt
+ * function implementation, specifically designed for use in
+ * Windows environments where the standard Unix `getopt` might
+ * not be available.
+ */
 
+// Index of the next element to be processed in argv
 extern int optind;
+
+// Pointer to the argument of the current option, if any
 extern char *optarg;
+
+// Flag for enabling error messages: 1 enables, 0 disables
 extern int opterr;
 
-int getopt (int argc, char *argv[], const char *optstring);
+/**
+ * Parses command-line options.
+ * @param argc The argument count.
+ * @param argv The argument vector.
+ * @param optstring A string containing the legitimate option characters.
+ * @return The option character if an option was successfully found, 
+ *         '?' if an option was not found, or -1 if there are no more options to process.
+ */
+int getopt(int argc, char *argv[], const char *optstring);
 
-#endif
+#endif // __GETOPT_W32_H__
